0ad8e59d60a83603e93456a4814b72b231773dd8
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2020-08-28 20:31:16 -0400

    Create 0831HelloWorld.java

---
+++ CSC-115-C01/0831HelloWorld.java
@@ -0,0 +1,12 @@
+// Carrie Krueger
+// 8/31/2020
+// This program writes a message to the screen.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World!"); //writes to the screen
+
+    }
+}
+ 
