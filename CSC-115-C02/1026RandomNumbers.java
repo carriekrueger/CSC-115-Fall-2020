66e51c6453bf66d76d059a8da9c9590a2fe9a686
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2020-10-26 13:39:09 -0400

    Create 1026RandomNumbers.java

---
+++ CSC-115-C02/1026RandomNumbers.java
@@ -0,0 +1,69 @@
+// Carrie Krueger
+// 10-26-20
+// Using the Random class to make random numbers
+
+import java.util.Random;
+// we are going to use the premade Java class Random
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        
+        makeRandoms();  // method for making and printing random numbers 
+    }
+    
+    // this method looks at making random integers in a range 
+    public static void makeRandoms()  {
+        
+        
+        // HOW TO MAKE A RANDOM NUMBER USING THE RANDOM CLASS
+        
+        //Step 1: Instantiate (create) a Random object
+        // (make an instance of the Random class)
+        // This is NOT the random number, but this is what we will use to
+        // get the random number 
+        
+        Random rand = new Random();
+        
+        // Step 2: use the .nextInt(int) method to generate a random integer
+        // in a range
+        
+        // integer generated is 0 to value - 1 (exclusive of value)
+        
+        int randomNumber = rand.nextInt(100);   // gives a number 0 - 99
+        System.out.println(randomNumber);
+        
+        // How to get a number between 1 and 100? 
+        
+        randomNumber = rand.nextInt(101); // no, gives 0 - 100
+        
+        System.out.println(rand.nextInt(100) + 1); // yes! gives 1-100
+                                    // 0 - 99 + 1 = 1 - 100
+                                    
+        
+        // How about a random number between 75-100?
+        
+        int randomNumber2 = rand.nextInt(26) + 75;
+                                     // 0 - 25 + 75 = 75 - 100
+                                     
+        
+        // Formula/Rule???
+        // # inside () = number of random numbers in the range
+        //             = max # - min # + 1
+        
+        // # added on outside = starting number (minimum number in range)
+        
+        
+        // How about a number between 20 and 50? 
+        
+        randomNumber2 = rand.nextInt(31) + 20;
+                                  // 0 - 30 + 20 = 20 - 50
+        
+        
+        // Live Code Demo in Blackboard 
+        
+        
+ 
+        
+        
+    }
+}
