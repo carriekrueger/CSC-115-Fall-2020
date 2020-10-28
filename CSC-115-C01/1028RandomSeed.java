9f1ed07aaa54680ea8068e0605746250952ec9b1
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2020-10-28 11:21:56 -0400

    Update 1028RandomSeed.java

--- CSC-115-C01/1028RandomSeed.java
+++ CSC-115-C01/1028RandomSeed.java
@@ -28,6 +28,23 @@ public class RandomSeed {
         int rand1 = rand.nextInt(100);  // between 0 - 99
         System.out.println("Random number 1 = " + rand1);
         
+        // use the same seed, you get the same number! 
+        
+        // 10-50 
+        int rand2 = rand.nextInt(41) + 10;
+        System.out.println(rand2);
+        
+        // Random chars... ASCII values/table
+        // Uppercase letter: 65-90
+        // need to cast to char
+        
+        int rand3 = rand.nextInt(26) + 65;
+        System.out.println("Random number 3 = " + rand3);
+        char randomChar = (char)rand3;
+        System.out.println("Character = " + randomChar);
+
+        
+        
         
     }
 }
